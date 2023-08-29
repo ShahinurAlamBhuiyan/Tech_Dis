1fdb39c1679d27cebe30054e4cdc7946a5f7a9a5
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-08-29 22:13:51 +0600

    nothing change

--- database/connect.js
+++ database/connect.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+
+
 async function connect() {
   await mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
